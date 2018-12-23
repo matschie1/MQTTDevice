276096e2ff97a160a8811c582e5b6243c6ebdec4
Peter de Lange <lange@dbis.rwth-aachen.de>
2018-12-23 15:03:27 +0100

    fix: 404 is correctly thrown when page is not existing
    and autoformat

--- 6_WEB.ino
+++ 6_WEB.ino
@@ -1,46 +1,50 @@
-void handleRoot(){
-  server.sendHeader("Location", "/index.html",true);   //Redirect to our html web page
-  server.send(302, "text/plane","");
+void handleRoot() {
+  server.sendHeader("Location", "/index.html", true);  //Redirect to our html web page
+  server.send(302, "text/plane", "");
 }
 
-void handleWebRequests(){
-  if(loadFromSpiffs(server.uri())) { return; }
+void handleWebRequests() {
+  if (loadFromSpiffs(server.uri())) {
+    return;
+  }
   String message = "File Not Detected\n\n";
   message += "URI: ";
   message += server.uri();
   message += "\nMethod: ";
-  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
   message += "\nArguments: ";
   message += server.args();
   message += "\n";
-  for (uint8_t i=0; i<server.args(); i++){
-    message += " NAME:"+server.argName(i) + "\n VALUE:" + server.arg(i) + "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " NAME:" + server.argName(i) + "\n VALUE:" + server.arg(i) + "\n";
   }
   server.send(404, "text/plain", message);
 }
 
 
-bool loadFromSpiffs(String path){
+bool loadFromSpiffs(String path) {
   String dataType = "text/plain";
-  if(path.endsWith("/")) path += "index.htm";
+  if (path.endsWith("/")) path += "index.html";
 
-  if(path.endsWith(".src")) path = path.substring(0, path.lastIndexOf("."));
-  else if(path.endsWith(".html")) dataType = "text/html";
-  else if(path.endsWith(".htm")) dataType = "text/html";
-  else if(path.endsWith(".css")) dataType = "text/css";
-  else if(path.endsWith(".js")) dataType = "application/javascript";
-  else if(path.endsWith(".png")) dataType = "image/png";
-  else if(path.endsWith(".gif")) dataType = "image/gif";
-  else if(path.endsWith(".jpg")) dataType = "image/jpeg";
-  else if(path.endsWith(".ico")) dataType = "image/x-icon";
-  else if(path.endsWith(".xml")) dataType = "text/xml";
-  else if(path.endsWith(".pdf")) dataType = "application/pdf";
-  else if(path.endsWith(".zip")) dataType = "application/zip";
-  
+  if (path.endsWith(".src")) path = path.substring(0, path.lastIndexOf("."));
+  else if (path.endsWith(".html")) dataType = "text/html";
+  else if (path.endsWith(".htm")) dataType = "text/html";
+  else if (path.endsWith(".css")) dataType = "text/css";
+  else if (path.endsWith(".js")) dataType = "application/javascript";
+  else if (path.endsWith(".png")) dataType = "image/png";
+  else if (path.endsWith(".gif")) dataType = "image/gif";
+  else if (path.endsWith(".jpg")) dataType = "image/jpeg";
+  else if (path.endsWith(".ico")) dataType = "image/x-icon";
+  else if (path.endsWith(".xml")) dataType = "text/xml";
+  else if (path.endsWith(".pdf")) dataType = "application/pdf";
+  else if (path.endsWith(".zip")) dataType = "application/zip";
+
+  if (!SPIFFS.exists(path.c_str())) {
+    return false;
+  }
   File dataFile = SPIFFS.open(path.c_str(), "r");
   if (server.hasArg("download")) dataType = "application/octet-stream";
   if (server.streamFile(dataFile, dataType) != dataFile.size()) {}
-
   dataFile.close();
   return true;
 }
@@ -52,7 +56,7 @@ void mqttreconnect() {
         actors[i].mqtt_subscribe();
         yield();
       }
-    inductionCooker.mqtt_subscribe();
+      inductionCooker.mqtt_subscribe();
     }
   }
 }
@@ -62,15 +66,15 @@ void mqttcallback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Topic: ");
   Serial.println(topic);
   Serial.print("Payload: ");
-  
+
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   } Serial.println(" ");
-    char payload_msg[length];
-    for (int i = 0; i < length; i++) {
-       payload_msg[i] = payload[i];
-    }
-        
+  char payload_msg[length];
+  for (int i = 0; i < length; i++) {
+    payload_msg[i] = payload[i];
+  }
+
   if (inductionCooker.mqtttopic == topic) {
     Serial.println("passing mqtt to induction");
     inductionCooker.handlemqtt(payload_msg);
@@ -82,5 +86,5 @@ void mqttcallback(char* topic, byte* payload, unsigned int length) {
       actors[i].handlemqtt(payload_msg);
     }
     yield();
-  }      
+  }
 }
