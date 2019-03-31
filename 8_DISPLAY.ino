6c1634570dbae2ac249c9274b6a46109e0c63b4f
Peter de Lange <lange@dbis.rwth-aachen.de>
2019-03-31 17:05:12 +0200

    nicer display of temperature

--- 8_DISPLAY.ino
+++ 8_DISPLAY.ino
@@ -1,11 +1,16 @@
 void drawDisplayContent(void) {
   display.clearDisplay();
-  display.setTextSize(2);
+  display.setTextSize(3);
   display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.println(F("Temperatur"));
-  display.setTextColor(BLACK, WHITE); // Draw 'inverse' text
-  display.print(ptSensors[0].value);
-  display.print(" C");
+  display.setCursor(2, 7);
+  char buf[4];
+  dtostrf(ptSensors[0].value, 2, 2, buf);
+  display.print(buf);
+  display.setTextSize(1);
+  display.print(" ");
+  display.setTextSize(2);
+  display.write(247); // deg symbol
+  display.setTextSize(3);
+  display.print("C");
   display.display();
 }
